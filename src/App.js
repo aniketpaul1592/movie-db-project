00f47d7310e8d784092b3d2f1190c5e6543af569
ANIKET PAUL <aniketpaul1592@gmail.com>
2017-08-14 12:35:10 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,11 +6,23 @@ import Main from './main.js'
 class App extends Component {
 	constructor(props){
 		super(props);
+    this.state = {
+      searchTerm: ''
+    }
+    this.updateSearch = this.updateSearch.bind(this)
 	}
+
+  updateSearch(event){
+    this.setState({
+      searchTerm:event.target.value
+    });
+  }
+
   render() {
     return (
       <div className="App">
-          <Header/>
+          <Header updateSearch={this.updateSearch}/>
+          <Main searchTerm={this.state.searchTerm}/>
       </div>
     );
   }
