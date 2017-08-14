b74554875f552b72d525a2a09ffd0f48f38145ea
ANIKET PAUL <aniketpaul1592@gmail.com>
2017-08-14 11:51:16 +0530

    Making Parent Component intelligent and child component dumb

---
+++ header.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import logo from './logo.png';
+import {Link} from 'react-router-dom';
+import Main from './main.js';
+import FontAwesome from 'react-fontawesome';
+
+const marginSpace = {
+	marginRight: 15,
+}
+
+class Header extends Component{
+	constructor(props){
+		super(props);
+	}
+	render(){
+		return(
+		<div className="headerLayoutApp">
+		<Link to="/"><img src={logo} className="logo" align="left"/></Link>     
+          <div className = "shiftRight">
+          	<Link to="/" style = {marginSpace}>Popular</Link>
+          	<Link to="/favorite" style = {marginSpace}>Favourites</Link>
+          	<input type = "text" onChange={this.props.updateSearch}/><FontAwesome name='search'/>
+          </div>
+        </div>
+		);
+	}
+}
+
+export default Header;
